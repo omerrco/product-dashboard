14056bd34c9c7283f02dcc39074b27a2248fd267
Omer Colak <omerrcolak@outlook.com>
2026-06-16 17:20:20 +0300

    feat: add new helper functions

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -9,3 +9,27 @@ export function setParam(params: URLSearchParams, key: string, value: string) {
 
   return nextParams;
 }
+
+export function formatCurrency(value: number) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    maximumFractionDigits: 2,
+  }).format(value);
+}
+
+export function formatDate(value: string) {
+  return new Intl.DateTimeFormat("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  }).format(new Date(value));
+}
+
+export function getInventoryValue(price: number, stock: number) {
+  return price * stock;
+}
+
+export function formatTitle(value: string) {
+  return value.replace(/\b\w/g, (char) => char.toUpperCase());
+}
